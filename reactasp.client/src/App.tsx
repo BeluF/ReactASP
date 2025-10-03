17237acaad56da79787dd16755528ac94df0e0c2
Belén Fornari <ma.belen.fornari@gmail.com>
2025-10-03 10:07:30 -0300

    add: estado-ganador

--- reactasp.client/src/App.tsx
+++ reactasp.client/src/App.tsx
@@ -1,17 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function Cuadrado({ valor, cuadradoClick }) {
+function Cuadrado({ valor, cuadradoClick, esGanador }: { valor: string | null; cuadradoClick: () => void; esGanador: boolean }) {
     return (
-        <button className="cuadrado" onClick={cuadradoClick}>
-            {valor }
+        <button className={`cuadrado ${esGanador ? 'ganador' : ''}`} onClick={cuadradoClick}>
+            {valor}
         </button>)
 }
 
-function Tablero({ xIsNext, cuadrado, enJuego }) {
-    function handleClick(i) {
+function Tablero({ xIsNext, cuadrado, enJuego }: { xIsNext: boolean; cuadrado: (string | null)[]; enJuego: (cuadrado: (string | null)[]) => void }) {
+    const resultado = calcularGanador(cuadrado);
+    const ganador = resultado ? resultado.ganador : null;
+    const lineaGanadoras = resultado ? resultado.lineas : [];
+
+    function handleClick(i: number) {
         if (calcularGanador(cuadrado) || cuadrado[i]) {
             return;
         }
@@ -26,68 +29,50 @@ function Tablero({ xIsNext, cuadrado, enJuego }) {
 
     }
 
-    const ganador = calcularGanador(cuadrado);
-    let estado;
+    let estado, claseEstado = "";
 
     if (ganador) {
         estado = 'Ganador: ' + ganador;
+        claseEstado = "estado-ganador";
+
     } else {
         estado = 'Siguiente jugador: ' + (xIsNext ? 'X' : 'O');
     }
 
-return (
-    <>
-        <div className="status">{estado}</div>
-        <div className="board-row">
-            <Cuadrado valor={cuadrado[0]} cuadradoClick={() => handleClick(0)} />
-            <Cuadrado valor={cuadrado[1]} cuadradoClick={() => handleClick(1)} />
-            <Cuadrado valor={cuadrado[2]} cuadradoClick={() => handleClick(2)} />
-        </div>
-        <div className="board-row">
-            <Cuadrado valor={cuadrado[3]} cuadradoClick={() => handleClick(3)} />
-            <Cuadrado valor={cuadrado[4]} cuadradoClick={() => handleClick(4)} />
-            <Cuadrado valor={cuadrado[5]} cuadradoClick={() => handleClick(5)} />
-        </div>
-        <div className="board-row">
-            <Cuadrado valor={cuadrado[6]} cuadradoClick={() => handleClick(6)} />
-            <Cuadrado valor={cuadrado[7]} cuadradoClick={() => handleClick(7)} />
-            <Cuadrado valor={cuadrado[8]} cuadradoClick={() => handleClick(8)} />
-        </div>
-    </>
-);
+    return (
+        <>
+            <div className="estado"><p className={`estadoP ${claseEstado}`}>{estado}</p></div>
+            <div className="board-row">
+                <Cuadrado valor={cuadrado[0]} cuadradoClick={() => handleClick(0)} esGanador={lineaGanadoras.includes(0)} />
+                <Cuadrado valor={cuadrado[1]} cuadradoClick={() => handleClick(1)} esGanador={lineaGanadoras.includes(1)} />
+                <Cuadrado valor={cuadrado[2]} cuadradoClick={() => handleClick(2)} esGanador={lineaGanadoras.includes(2)} />
+            </div>
+            <div className="board-row">
+                <Cuadrado valor={cuadrado[3]} cuadradoClick={() => handleClick(3)} esGanador={lineaGanadoras.includes(3)} />
+                <Cuadrado valor={cuadrado[4]} cuadradoClick={() => handleClick(4)} esGanador={lineaGanadoras.includes(4)} />
+                <Cuadrado valor={cuadrado[5]} cuadradoClick={() => handleClick(5)} esGanador={lineaGanadoras.includes(5)} />
+            </div>
+            <div className="board-row">
+                <Cuadrado valor={cuadrado[6]} cuadradoClick={() => handleClick(6)} esGanador={lineaGanadoras.includes(6)} />
+                <Cuadrado valor={cuadrado[7]} cuadradoClick={() => handleClick(7)} esGanador={lineaGanadoras.includes(7)} />
+                <Cuadrado valor={cuadrado[8]} cuadradoClick={() => handleClick(8)} esGanador={lineaGanadoras.includes(8)} />
+            </div>
+        </>
+    );
 }
-export default function Juego(){
+
+export default function Juego() {
     const [historial, setHistorial] = useState([Array(9).fill(null)]);
     const [movimientoActual, setMovimientoActual] = useState(0);
 
     const xIsNext = (movimientoActual % 2) === 0;
     const cuadrado = historial[movimientoActual];
 
-    function handlePlay(proximoCuadrado) {
+    function handlePlay(proximoCuadrado: (string | null)[]) {
         const nuevoHistorial = [...historial.slice(0, movimientoActual + 1), proximoCuadrado];
         setHistorial(nuevoHistorial);
         setMovimientoActual(nuevoHistorial.length - 1);
     }
-
-    function irAMovimiento(movimiento) {
-        setMovimientoActual(movimiento);
-    }
-
-    const movimientos = historial.map((cuadrado, movimiento) => {
-        let descripcion;
-
-        if(movimiento > 0){
-            descripcion = 'Ir al movimiento #' + movimiento;
-        }
-        else{
-            descripcion = 'Ir al inicio del juego';
-        }
-        return (
-            <li key={movimiento}>
-                <button onClick={() => irAMovimiento(movimiento)}>{descripcion}</button>
-            </li>
-        )
-    });
 
     return (
         <div className="juego">
@@ -95,13 +80,13 @@ export default function Juego(){
                 <Tablero xIsNext={xIsNext} cuadrado={cuadrado} enJuego={handlePlay} />
             </div>
             <div className="juego-info">
-                <ol>{movimientos}</ol>
+                <ol>...</ol>
             </div>
         </div>
     );
 }
 
-function calcularGanador(cuadrado) {
+function calcularGanador(cuadrado: (string | null)[]) {
     const lineas = [
         [0, 1, 2],
         [3, 4, 5],
@@ -116,7 +101,7 @@ function calcularGanador(cuadrado) {
     for (let i = 0; i < lineas.length; i++) {
         const [a, b, c] = lineas[i];
         if (cuadrado[a] && cuadrado[a] === cuadrado[b] && cuadrado[a] === cuadrado[c]) {
-            return cuadrado[a];
+            return { ganador: cuadrado[a], lineas: [a, b, c] };
         }
     }
     return null;
